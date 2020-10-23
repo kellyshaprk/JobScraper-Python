7aa170acce3a2b013f461d579c6a21237ee1f10b
Kelly <kelly.shpark@gmail.com>
2020-10-23 00:09:40 -0400

    render template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScraper")
 
@@ -6,11 +6,10 @@ app = Flask("SuperScraper")
 # 데코레이터는 바로 밑에 명시된 함수를 실행시킴. 때문에 route의 페이지 이름과 함수명이 같을 필요는 없음 
 @app.route("/")
 def home():
-  return "Hello! Welcome to new world!"
+  return render_template("home.html")
 
-@app.route("/contact")
-def contact():
-  return "Contact Me!"
+
+
 
 #repl에서 사용하기 위한 host주소임, local에선 안먹힘
 app.run(host="0.0.0.0")
