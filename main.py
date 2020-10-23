7f13fc79c4c001bb3e9aaadd42ff75baa7abd058
Kelly <kelly.shpark@gmail.com>
2020-10-22 23:51:44 -0400

    flask

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from flask import Flask
 
-from indeed import get_jobs as get_indeed_jobs
-#from so import get_jobs as get_so_jobs
-from save import save_to_file
+app = Flask("SuperScraper")
 
-indeed_jobs = get_indeed_jobs()
-#so_jobs = get_so_jobs()
-save_to_file(indeed_jobs)
+# @<- decorator
+# 데코레이터는 바로 밑에 명시된 함수를 실행시킴. 때문에 route의 페이지 이름과 함수명이 같을 필요는 없음 
+@app.route("/")
+def home():
+  return "Hello! Welcome to new world!"
 
+@app.route("/contact")
+def contact():
+  return "Contact Me!"
+
+#repl에서 사용하기 위한 host주소임, local에선 안먹힘
+app.run(host="0.0.0.0")
