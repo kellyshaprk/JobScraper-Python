2effa286d3c2449752b79d05748eb98ae3989162
Kelly <kelly.shpark@gmail.com>
2020-10-23 14:11:44 -0400

    save the results to db

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+#request를 통해서 home.html에서 사용자가 입력한 단어를 추출하고 사용할 수 있음
+from flask import Flask, render_template, request, redirect
+
+from scraper import get_jobs as get_indeed_jobs
+#from so import get_jobs as get_so_jobs
+#from save import save_to_file
+
+#indeed_jobs = get_indeed_jobs()
+#so_jobs = get_so_jobs()
+#save_to_file(indeed_jobs)
 
 app = Flask("SuperScraper")
+db = {}
 
 # @<- decorator
 # 데코레이터는 바로 밑에 명시된 함수를 실행시킴. 때문에 route의 페이지 이름과 함수명이 같을 필요는 없음 
@@ -8,8 +18,31 @@ app = Flask("SuperScraper")
 def home():
   return render_template("home.html")
 
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  if word:
+    word = word.lower()
 
+    #job 정보를 db에 저장해놓고, 동일한 요청이 들어오면 db에서 읽어서 보여주고 아니면 새로 검색하기
+    fromDb = db.get(word)
+    if fromDb:
+      jobs = fromDb
+    else:
+      jobs = get_indeed_jobs(word)
+      db[word] = jobs
+  else:
+    return redirect("/")
+  return render_template(
+    "report.html"
+  , searchingby=word
+  , resultNumber=len(jobs)
+  )
 
+# <> means placeholder
+@app.route("/<username>") 
+def potato(username):
+  return f"Hey, {username}"
 
 #repl에서 사용하기 위한 host주소임, local에선 안먹힘
 app.run(host="0.0.0.0")
---
+++ scraper.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+limit = 50
+
+
+def extract_indeed_pages(url):
+  result = requests.get(url)
+  soup = BeautifulSoup(result.text, "html.parser")
+
+  pagination = soup.find("ul", {"class":"pagination-list"})
+  #print(pagination)
+
+  links = pagination.find_all("a")
+  pages = []
+  for link in links[0:-1]:
+    pages.append(int(link.string))
+
+  max_page = pages[-1]
+  return max_page
+
+def extract_jobs(html):
+    title = html.find("h2", {"class", "title"}).find("a")["title"]
+    company = html.find("span", {"class":"company"})
+    
+    if company:
+      company_anchor = company.find("a")
+      if company_anchor is not None:
+        company = str(company_anchor.string)
+      else:
+        company = str(company.string)
+      company = company.strip()
+    else:
+      company = None
+    location = html.find("div", {"class":"recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+      'title':title, 
+      'company':company, 
+      'location' : location, 
+      'link':f"https://ca.indeed.com/viewjob?jk={job_id}"
+    }
+
+def extract_indeed_jobs(last_page, url):
+  jobs = []
+  for page in range(last_page):
+    print(f"Scrapping indeed page {page}")
+    result = requests.get(f"{url}&start={page*limit}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+      job = extract_jobs(result)
+      jobs.append(job)
+  return jobs
+
+def get_jobs(word):
+  url = f"https://ca.indeed.com/jobs?q={word}&l=alberta&limit={limit}"
+  last_page = extract_indeed_pages(url)
+  jobs = extract_indeed_jobs(last_page, url)
+  return jobs
